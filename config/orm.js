6e7db2be8bdad0b4baf960149dde86c74c599f89
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-08 06:33:02 +1100

    helper functions to add ?? in query

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,13 @@
 const connection = require('../connection.js');
-
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = (num) => {
+    const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push('?');
+    }
+  
+    return arr.toString();
+  };
+  
+  
