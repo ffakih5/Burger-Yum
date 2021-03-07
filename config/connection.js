4142f88dd356305a2388d968bcd408868da3ddfe
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-08 06:19:53 +1100

    adding connection.js code from class activity- set up connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// Set up MySQL connection.
+
+
+const connection = mysql.createConnection({
+ 
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
