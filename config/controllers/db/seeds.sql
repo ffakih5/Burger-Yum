babc4f54f5437ecdf87188f16bbec878583cb35c
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-08 07:46:56 +1100

    seeds file content

---
+++ config/controllers/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+
+VALUES 
+("Hamburger"),
+("Cheeseburger")
+("BBQ Bacon Cheeseburger")
+("Southern Kentucky Fried Chicken Burger")
+("Veggie Burger");
