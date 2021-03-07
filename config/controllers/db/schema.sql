27a7f6f76de2a449b6c60e0161ff7eb85c3a1730
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-08 07:15:39 +1100

    CREATE TABLE

--- config/controllers/db/schema.sql
+++ config/controllers/db/schema.sql
@@ -2,9 +2,13 @@ DROP DATABASE IF EXISTS burgers_db;
 
 USE burgers_db; 
 
-
-id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	sleepy BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
+
 );
+
+
+
