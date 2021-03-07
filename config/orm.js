6f99d71f9599dd01b643e1dc0518009aead62eb0
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-08 06:35:53 +1100

    for loop through keys and push key/value as string

--- config/orm.js
+++ config/orm.js
@@ -13,4 +13,19 @@ const printQuestionMarks = (num) => {
   const objToSql = (ob) => {
     const arr = [];
   
-    
+    // Loop through the keys and push the key/value as a string int arr
+    for (const key in ob) {
+      let value = ob[key];
+      // Check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+          value = `'${value}'`;
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(`${key}=${value}`);
+      }
+    }
+  
+   
