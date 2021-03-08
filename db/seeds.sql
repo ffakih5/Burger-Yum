ceea10ef7c487e710b827a413445a3bc4adfe8ec
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-09 06:00:51 +1100

    trying false/true with burger input values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO burgers_db.burgers (burger_name, devoured)
 
 VALUES 
-("Hamburger", 0),
-("Cheeseburger", 0),
-("BBQ Bacon Cheeseburger", 0),
-("Southern Kentucky Fried Chicken Burger", 0),
-("Veggie Burger", 0);
+("Hamburger", false),
+("Cheeseburger", true),
+("BBQ Bacon Cheeseburger", false),
+("Southern Kentucky Fried Chicken Burger", false),
+("Veggie Burger", false);
