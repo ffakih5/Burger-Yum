6cc3fc7d27eb60588c04d3bd7ea777df755fcfd6
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-10 20:34:25 +1100

    recreated sql files

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Zinger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Burger');
