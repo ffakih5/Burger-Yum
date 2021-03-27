4e3bb1a9b4b38672ccd88261ad422fab06ee7094
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-27 19:59:39 +1100

    editing space parentheses now with closing bracket l27

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,8 +24,7 @@ router.post('/api/burgers', (req, res) => {
   burger.create(['burger_name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
-  }
-  );
+  });
 });
 
 router.put('/api/burgers/:id', (req, res) => {
