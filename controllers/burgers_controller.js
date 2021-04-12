03a41c2d06642b2a9fbbeddd11c9a1f2ad0e15c7
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-12 14:59:27 +1000

    post requests to create and add burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,48 +17,19 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/burgers', (req, res) => {
-  /*let devoured = 0;
-  if (req.body.devoured === 'true') {
-    devoured = 1;
-  }*/
-  burger.create(['burger_name', 'devour'], [req.body.name, req.body.devour], (result) => {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  }
-  );
-});
-
-router.put('/api/burgers/:id', (req, res) => {
-  const condition = `id = ${req.params.id}`;
-
-  console.log('condition', condition);
-
-  burger.update(
-    {
-      devour: req.body.devour,
-    },
-    condition,
-    (result) => {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    }
-  );
-});
-
-router.delete('/api/burgers/:id', (req, res) => {
-  const condition = `id = ${req.params.id}`;
-
-  burger.delete(condition, (result) => {
-    if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
+  burger.addBurger(req.body.burger_name, (result) => {
     res.status(200).end();
   });
 });
 
-// Export routes for server.js to use.
+router.post('/api/burgers/:id', (req, res) => {
+  burger.updateBurger(req.body.devoured, req.params.id, (result) => {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
 module.exports = router;
