3a13bc4c834f7e76f23f35d7286eb362416ef259
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-12 14:51:11 +1000

    reverting files

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
+// Set up MySQL connection.
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'myRootPassword',
+  database: 'burgers_db',
 });
 
 // Make connection.
