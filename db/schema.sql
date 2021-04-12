481f0d0618acbbdf8c8ed73cd7fae7ece42e546d
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-12 14:49:27 +1000

    editing db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,10 @@
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourRootPassword';
+CREATE DATABASE burgers_db;
 
-DROP DATABASE IF EXISTS burgers_db;
+USE burgers_db;
 
-CREATE DATABASE burger_db;
-
-USE burger_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devour BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+    PRIMARY KEY (id)
 );
