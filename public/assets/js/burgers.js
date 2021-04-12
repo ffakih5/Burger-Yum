9ed30f1c215edf1e8a0fa1a69e6fbdb431f239e4
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-12 15:02:47 +1000

    event listeners for each button; submitting burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,90 +3,64 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.info('DOM Loaded');
   }
 
-  const changeDevourBtns = document.querySelectorAll('.change-devour');
+  const devourBtn = document.querySelectorAll('.devour-button');
 
-  if (changeDevourBtns) {
-    changeDevourBtns.forEach((button) => {
-      button.addEventListener('click', (e) => {
-        // Grabs the id of the element that goes by the name, "id"
-        const id = e.target.getAttribute('data-id');
-        const newDevour = e.target.getAttribute('data-newdevour');
-
-        const newDevourStatus = {
-          devoured: newDevour,
-        };
-
-        fetch(`/api/burgers/${id}`, {
-          method: 'PUT',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-
-          body: JSON.stringify(newDevourStatus),
-        }).then((response) => {
-          // Empty the form
-          if (response.ok) {
-            console.log(`Status Updated: ${newDevour}`);
-            location.reload("/");
-          } else {
-            alert('Could not update devour status')
-            console.log(response)
-          }
-        });
-      });
-    });
-  }
-  const createBurgerBtn = document.getElementById('start-form');
-
-  if (createBurgerBtn) {
-    createBurgerBtn.addEventListener('submit', (e) => {
+  devourBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
       e.preventDefault();
 
-      // Grabs the value of the textarea that goes by the name, "quote"
-      const newBurger = {
-        name: document.getElementById('brgr').value.trim(),
-        devour: document.getElementById('devoured').checked,
+      const dataId = e.target.getAttribute('data-id');
+      const id = parseInt(dataId);
+
+      const isDevoured = {
+        devoured: true,
       };
 
-      // Send POST request to create a new quote
-      fetch('/api/burgers', {
-        method: 'POST',
+      fetch(`/api/burgers/${id}`, {
+        method: 'PUT',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify(isDevoured),
+      }).then((response) => {
+        if (response.ok) {
+          console.log(`Status Updated: ${isDevoured}`);
+          location.reload('/');
+        } else {
+          alert('Could not update devour status')
+        }
 
-        // make sure to serialize the JSON body
-        body: JSON.stringify(newBurger),
-      }).then(() => {
-        // Empty the form
-        document.getElementById('brgr').value = '';
-
-        // Reload the page so the user can see the new quote
-        console.log('Created a new burger!');
-        location.reload();
       });
     });
-  }
 
-  const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
+  });
 
-  // Set up the event listeners for each delete button
-  deleteBurgerBtns.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
+  const submitBurger = document.getElementById('start-form');
 
-      // Send the delete request
-      fetch(`/api/burgers/${id}`, {
-        method: 'DELETE',
-      }).then((res) => {
-        console.log(res);
-        console.log(`Deleted burger: ${id}`);
+  submitBurger.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const burgerName = document.getElementById('burger-select').value.trim();
+    if (burgerName === '') {
+      return;
+    }
+    const newBurger = {
+      burger_name: burgerName
+    };
 
-        // Reload the page
-        location.reload();
-      });
+    fetch('/api/burgers', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(newBurger),
+    }).then(() => {
+      document.getElementById('burger-select').value = '';
+      console.log('Created a new burger!');
+      location.reload();
     });
   });
+
 });
