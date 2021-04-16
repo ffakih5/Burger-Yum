5d7b38ad490af7f68d042e4e79638a339eec50f6
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-16 14:12:28 +1000

    JAWSDB not JAWSDW

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 let connection;
 
-if (process.env.JAWSDW_URL) {
-  connection = mysql.createConnection(process.env.JAWSDW_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
